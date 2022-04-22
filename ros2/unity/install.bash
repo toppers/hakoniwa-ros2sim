ca5d991a21dabdd2f98ba4cacc35f92185790884
Takashi Mori <kanetugu2015@gmail.com>
2022-04-23 08:51:26 +0900

    updated for unity 2021.3.0f1

---
+++ ros2/unity/install.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#!/bin/bash
+
+CURR=`pwd`
+
+cd tb3/Assets
+if [ -d Model ]
+then
+	:
+	echo "Model is already installed."
+else
+	wget https://github.com/toppers/hakoniwa-ros-simulator/releases/download/v1.0.0/Model.tar.gz
+	tar xzvf Model.tar.gz
+	rm -f Model.tar.gz
+fi
+
+if [ -d Plugin ]
+then
+	echo "Plugin is already installed."
+else
+	wget https://github.com/toppers/hakoniwa-ros-simulator/releases/download/v1.0.0/Plugin.tar.gz
+	tar xzvf Plugin.tar.gz
+	rm -f Plugin.tar.gz
+fi
+
+if [ -d Resources/Hakoniwa/Robots ]
+then
+	echo "Resources is already installed."
+else
+	mkdir -p Resources/Hakoniwa/Robots
+	cp -p Prefabs/Robots/TB3RoboModel.prefab*  Resources/Hakoniwa/Robots/
+fi
+
+
