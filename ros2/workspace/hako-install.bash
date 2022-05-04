7fa15fe836c6155e6264c3c2e029e429fc834bad
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-05-04 18:15:14 +0900

    change filename from install.bash to create_ros2unity_info

--- ros2/workspace/hako-install.bash
+++ ros2/workspace/hako-install.bash
@@ -6,9 +6,9 @@ cd ../..
 
 if [ ${OS_TYPE} = "Mac" ]
 then
-	bash utils/install.bash ros2 ros2/unity/tb3 settings/tb3 config
+	bash utils/create_ros2unity_info.bash ros2 ros2/unity/tb3 settings/tb3 config
 else
-	bash utils/wsl2_install.bash ros2 ros2/unity/tb3 settings/tb3 config
+	bash utils/create_ros2unity_info_wsl2.bash ros2 ros2/unity/tb3 settings/tb3 config
 fi
 
 cd ros2/unity
--- utils/create_ros2unity_info_wsl2.bash
+++ utils/create_ros2unity_info_wsl2.bash
@@ -6,4 +6,4 @@ cat settings/tb3/ros2_search_file_path.txt | grep -v samples > tmp.txt
 CURDIR=`pwd`
 echo "${CURDIR}/ros2/workspace/src" >> tmp.txt
 mv tmp.txt settings/tb3/ros2_search_file_path.txt
-bash utils/install.bash $*
+bash utils/create_ros2unity_info.bash $*
