96663e3d70b7889b1bbb931713f70f3e73fbc684
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-05-06 16:40:55 +0900

    change workdir in container

--- utils/create_ros2unity_info_wsl2.bash
+++ utils/create_ros2unity_info_wsl2.bash
@@ -2,7 +2,7 @@
 
 export CORE_IPADDR=`cat /etc/resolv.conf  | grep nameserver | awk '{print $NF}'`
 
-cat settings/tb3/ros2_search_file_path.txt | grep -v samples > tmp.txt
+cat settings/tb3/ros2_search_file_path.txt | grep -v ros2sim > tmp.txt
 CURDIR=`pwd`
 echo "${CURDIR}/ros2/workspace/src" >> tmp.txt
 mv tmp.txt settings/tb3/ros2_search_file_path.txt
